d06f77355de35b203dc22a2402bcf48bab301904
Serhii Hurin <108822882+Hurinworld@users.noreply.github.com>
2023-06-19 15:40:10 +0300

    Delete online_shop directory

--- online_shop/src/main/java/com/serhiihurin/shop/online_shop/controller/OnlineShopRESTController.java
+++
@@ -1,9 +0,0 @@
-package com.serhiihurin.shop.online_shop.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/online_shop")
-public class OnlineShopRESTController {
-}
--- online_shop/src/main/java/com/serhiihurin/shop/online_shop/dao/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.serhiihurin.shop.online_shop.dao;
-
-import com.serhiihurin.shop.online_shop.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
