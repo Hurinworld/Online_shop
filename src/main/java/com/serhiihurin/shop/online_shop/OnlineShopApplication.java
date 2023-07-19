6918e72e488602c3583a1a04c2ddc69de3e5a2f0
Serhii Hurin <misterhurin@gmail.com>
2023-07-19 14:39:14 +0300

    Removed unnecessary imports

--- src/main/java/com/serhiihurin/shop/online_shop/OnlineShopApplication.java
+++ src/main/java/com/serhiihurin/shop/online_shop/OnlineShopApplication.java
@@ -2,8 +2,6 @@ package com.serhiihurin.shop.online_shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 //TODO create exception (later) and userPrincipal controller advices
 //TODO check urls for a hierarchy structure
